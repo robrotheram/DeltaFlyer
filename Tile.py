7dfc1f3bedbe2d8fafee98343135871e2f271769
robrotheram <robrotheram@gmail.com>
2016-11-07 20:30:47 +0000

    Woop the delta flyer lives

--- Tile.py
+++ Tile.py
@@ -14,8 +14,6 @@ class Tile:
     max_zoom = math.ceil((np.log((pixel_size/tile_size)) / np.log(2)))
     map_max_size = math.pow(2,max_zoom)*tile_size
 
-
-
     def __init__(self):
         with open('SERVER.json') as data_file:
             data = json.load(data_file)
@@ -23,69 +21,6 @@ class Tile:
         self.x = 'init!'
         self.rendered_chunks = {}
         #print(self.x)
-
-
-    def get_chunk_list(self, z,x,y):
-        list = []
-        bounds = math.pow(2,(self.max_zoom-z))*256
-        xb = bounds+(bounds*x)
-        yb = bounds+(bounds*y)
-        n_chucks_width = int((bounds)/256)
-        x = (xb/256)-n_chucks_width
-        y = (yb/256)-n_chucks_width
-        for i in range(0,(n_chucks_width*n_chucks_width),1):
-            ix = i % n_chucks_width
-            iy = math.floor(i / n_chucks_width)
-            list.append((ix+x,iy+y))
-        return list
-
-    def getchunk(self,z):
-        new_im = Image.new('RGB', (512,512))
-        imgsize = 32
-        width = 16
-
-        img_hash = hash(tuple(self.chunk_data.pop()["map"]))
-
-        if(img_hash in self.rendered_chunks):
-           # print("Found!!!")
-            return self.rendered_chunks[img_hash]
-        else:
-          #  print("rendering new chunk")
-            for i in range(0,(width*width),1):
-                x = i % width
-                y = math.floor(i / width)
-                im = Image.open(self.getimg(i))
-                im = im.resize((imgsize, imgsize), Image.BILINEAR)
-                new_im.paste(im, ((x*imgsize),(y*imgsize)))
-
-            self.rendered_chunks[img_hash]=new_im
-            return new_im
-
-    def get4chunks(self,z):
-        z = int(math.pow(2,(self.max_zoom -(z))))
-        width = z
-        imgsize = math.floor((500/z))
-        new_width = imgsize*z #
-
-        new_im = Image.new('RGB', (new_width,new_width))#
-        for i in range(0,(width*width),1):
-            x = i % width
-            y = math.floor(i / width)
-            im = self.getchunk(z)
-            im = im.resize((imgsize, imgsize), Image.BILINEAR)
-            new_im.paste(im, ((x*imgsize),(y*imgsize)))
-
-        new_im = new_im.resize((500, 500), Image.BILINEAR)#resize up to 512 to remove border
-
-        new_size = (512 , 512)
-        old_size = new_im.size
-        border = Image.new("RGB", new_size)   ## luckily, this is already black!
-        border.paste(new_im, (6,6))
-        return border
-
-
-
-
 
     def getimg(self, i):
         if (i >= len(self.chunk_data)):
@@ -125,39 +60,42 @@ class Tile:
         img_hash = hash(tuple(blocks))
         width =16;
         if(img_hash in self.rendered_chunks):
-           # print("Found!!!")
+            print("Found!!!")
             return self.rendered_chunks[img_hash]
         else:
-            #print("rendering new chunk")
+            print("rendering new chunk")
+            #blocks.reverse();
             for i in range(0,(width*width),1):
-                x = i % width
-                y = math.floor(i / width)
+                x = math.floor(i / width)
+                y = i % width
+
                 im = self.get_img(blocks[i])
                 img.paste(im, ((x*self.block_size),(y*self.block_size)))
             self.rendered_chunks[img_hash]=img
-            return img
+            return img#.rotate(270)#.transpose(Image.FLIP_LEFT_RIGHT)
 
     def rendertile(self,z,x,y):
         chunklist = self.get_chunk_list(z,x,y)
+        print(chunklist)
         chunk_width = math.pow(2,self.max_zoom-z);
         image_size = int(self.chunk_image_size/chunk_width)
         tile = Image.new('RGB', (self.chunk_image_size,self.chunk_image_size)) #512 x 512
-        #print(chunk_width)
-        #print(image_size)
+        print(chunk_width)
+        print(image_size)
 
         for i in range(0,len(chunklist),1):
             cl = chunklist[i]
             x = i % chunk_width
             y = math.floor(i / chunk_width)
-            #print("%d,%d"%(x,y))
+            print("%d,%d"%(x,y))
             res = list(filter(lambda x: x["location"]["x"] == cl[0] and x["location"]["z"] == cl[1], self.chunk_data))
             img = None
             if not (res):
-                #print("(%d,%d) Empty List"%(x,y))
+                print("(%d,%d) Empty List"%(x,y))
                 img = Image.new('RGB',(32,32))
             else:
                 chunk = (res.pop())
-                #print("(%d,%d) | Chunk: (%d,%d)"%(x,y,chunk["location"]["x"],chunk["location"]["z"]))
+                print("(%d,%d) | Chunk: (%d,%d)"%(x,y,chunk["location"]["x"],chunk["location"]["z"]))
                 blocks = chunk["map"]
                 img = self.render_chunk(blocks)
 
@@ -172,8 +110,7 @@ class Tile:
 
 if __name__ == "__main__":
      tile = Tile()
-     tile.rendertile(9,9,-11).show();
-     tile.rendertile(9,10,-11).show();
+     tile.rendertile(8,5,-6).show();
 
 
      # image = tile.get4chunks()
