1518d3c5a97a94fbdccd17b816f05528007599ca
rob <robrotheram@gmail.com>
2016-11-15 11:11:58 +0000

    Improved kafka performance

--- handlers/ingestHandler.py
+++ handlers/ingestHandler.py
@@ -1,13 +1,27 @@
 import tornado.web
 import json
+
+from concurrent.futures import ThreadPoolExecutor
 from kafka import KafkaProducer
+from tornado.concurrent import run_on_executor
 
 
 class IngestHandler(tornado.web.RequestHandler):
-    def post(self):
-        data = tornado.escape.json_decode(self.request.body)
-        producer = KafkaProducer(bootstrap_servers='192.168.1.173:9092')
+    executor = ThreadPoolExecutor(max_workers=100)
+    producer = KafkaProducer(bootstrap_servers='192.168.1.173:9092')
+
+    @run_on_executor
+    def background_task(self, data):
+        """ This will be executed in `executor` pool. """
+        data["meta"] = {"serverID":"testACCOUNT"};
         r = json.dumps(data)
-        producer.send('test', r).get(timeout=60)
+        self.producer.send('test', r).get(timeout=1)
+        return True
+
+    @tornado.gen.coroutine
+    def post(self):
+        """ Request that asynchronously calls background task. """
+        res = yield self.background_task(tornado.escape.json_decode(self.request.body))
         response = { 'message': 'Welcome to the coolest API on earth to kafka send!' }
         self.write(response)
+
